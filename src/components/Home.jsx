0734844e56ba1dbf86a8e3375dc9a79ac50887ea
rhea-vyte <rhealyn@vyte.dev>
2026-03-22 18:29:27 +0800

    REMOVED GIFT

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,6 @@ import Schedule from './Schedule'
 import RSVPSection from './RSVPSection'
 import LoveStory from './LoveStory'
 import DressCode from './DressCode'
-import GiftSection from './GiftSection'
 import FAQ from './FAQ'
 import SaveTheDateCounter from './SaveTheDateCounter'
 import Divider from './Divider'
@@ -65,9 +64,6 @@ const Home = ({ onOpenRSVP }) => {
           <LoveStory />
         </div>
       </div>
-
-      {/* Gift Section */}
-      <GiftSection />
 
       {/* FAQ Section */}
       <FAQ />
